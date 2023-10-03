1bc9bf8addc0e4ea9bdc6499d9d4a2eb4ad323f7
Han <yulmam1@naver.com>
2023-10-03 20:41:48 +0900

    controller 수정

---
+++ back/modak/src/main/java/com/example/modak/controller/LogInController.java
@@ -0,0 +1,38 @@
+package com.example.modak.controller;
+
+import com.example.modak.dto.LogInResultDto;
+import com.example.modak.dto.RegisterResultDto;
+import com.example.modak.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class LogInController {
+    private final UserService userService;
+
+
+    @Autowired
+    public LogInController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public LogInResultDto logIn() throws RuntimeException {
+        LogInResultDto logInResultDto = userService.logIn(id, password);
+
+        return logInResultDto;
+    }
+
+    @PostMapping("/register")
+    public RegisterResultDto register() {
+        RegisterResultDto registerResultDto = userService.register();
+
+        return registerResultDto;
+    }
+
+
+}
--- back/modak/src/main/java/com/example/modak/domain/Posts.java
+++ back/modak/src/main/java/com/example/modak/domain/Posts.java
@@ -1,4 +1,8 @@
 package com.example.modak.domain;
 
+
+@Getter
+@Entity
+@NoArgsConstructor
 public class Posts {
 }
