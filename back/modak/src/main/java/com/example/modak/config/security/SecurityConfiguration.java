5e380c877acdb99818151765a262a16c8e44b128
Han <yulmam1@naver.com>
2023-10-05 16:53:50 +0900

    security 추가

--- back/modak/src/main/java/com/example/modak/config/security/SecurityConfiguration.java
+++ back/modak/src/main/java/com/example/modak/config/security/SecurityConfiguration.java
@@ -1,16 +1,22 @@
 package com.example.modak.config.security;
 
 
+import com.example.modak.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-public class SecurityConfiguration  {
+//@EnableWebSecurity // Spring Security에 대한 디버깅 모드를 사용하기 위한 어노테이션 (default : false)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -19,11 +25,44 @@ public class SecurityConfiguration  {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic().disable() // REST API는 UI를 사용하지 않으므로 기본설정을 비활성화
 
+                .csrf().disable() // REST API는 csrf 보안이 필요 없으므로 비활성화
 
+                .sessionManagement()
+                .sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS) // JWT Token 인증방식으로 세션은 필요 없으므로 비활성화
+
+                .and()
+                .authorizeRequests() // 리퀘스트에 대한 사용권한 체크
+                .antMatchers("/sign-api/sign-in", "/sign-api/sign-up",
+                        "/sign-api/exception").permitAll() // 가입 및 로그인 주소는 허용
+                .antMatchers(HttpMethod.GET, "/product/**").permitAll() // product로 시작하는 Get 요청은 허용
+
+                .antMatchers("**exception**").permitAll()
+
+                .anyRequest().hasRole("ADMIN") // 나머지 요청은 인증된 ADMIN만 접근 가능
+
+                .and()
+                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class); // JWT Token 필터를 id/password 인증 필터 이전에 추가
     }
 
-
+    /**
+     * Swagger 페이지 접근에 대한 예외 처리
+     *
+     * @param webSecurity
+     */
+    @Override
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**",
+                "/swagger-ui.html", "/webjars/**", "/swagger/**", "/sign-api/exception");
+    }
 }
