14b194be2135b2965f3118c29380077dd87cd6a9
Han <yulmam1@naver.com>
2023-10-31 16:53:10 +0900

    PostController 추가

--- back/modak/src/main/java/com/example/modak/logIn/respository/PostsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.modak.logIn.respository;
-
-import com.example.modak.board.domain.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
