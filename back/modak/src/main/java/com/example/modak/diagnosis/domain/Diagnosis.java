52fb3dab873a78708da45f69f0ff916be3c11246
Han <yulmam1@naver.com>
2023-11-07 17:54:11 +0900

    feat : dignosis entity 수정

--- back/modak/src/main/java/com/example/modak/diagnosis/domain/Diagnosis.java
+++ back/modak/src/main/java/com/example/modak/diagnosis/domain/Diagnosis.java
@@ -1,15 +1,28 @@
 package com.example.modak.diagnosis.domain;
 
 import com.example.modak.logIn.domain.User;
+import lombok.*;
 
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table
 public class Diagnosis {
 
+    @Id
+    @GeneratedValue
     long id;
+    @Column
     long tutleNeckResult;
+    @Column
     long scoliosis;
+    @Column
     Date date;
     @ManyToOne
     User user;
