b22ceb0f5710f630d6b9d029d00281eadebf743a
Lee gueyeol <i1004gy@google.com>
2023-11-24 14:46:39 +0900

    update

--- front/src/routes/Login.js
+++ front/src/routes/Login.js
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useCallback } from "react";
 import AppLayout from "../components/AppLayout";
-import LoginForm from "../components/LoginForm";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./Login.css";
+import { useDispatch } from "react-redux";
+import useInput from "../hooks/useInput";
+import { loginAction } from "../reducers/user";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { isLoggedIn } = useSelector((state) => state.user);
+  const { logInLoading } = useSelector((state) => state.user);
+  const [uid, onChangeuid] = useInput("");
+  const [password, onChangePassword] = useInput("");
 
+  const onSumbitForm = useCallback(() => {
+    console.log(uid, password);
+    dispatch(loginAction({ uid, password }));
+    navigate("/");
+  }, [uid, password, dispatch]);
   return (
     <>
       <AppLayout>
         <div class="login-container">
           <h2>로그인</h2>
           <form>
-            <input type="text" placeholder="아이디" required />
+            <input
+              type="text"
+              placeholder="아이디"
+              value={uid}
+              onChange={onChangeuid}
+              required
+            />
             <br />
-            <input type="password" placeholder="비밀번호" required />
+            <input
+              type="password"
+              placeholder="비밀번호"
+              value={password}
+              onChange={onChangePassword}
+              required
+            />
             <br />
-            <button type="submit">로그인</button>
+            <button type="submit" onClick={onSumbitForm}>
+              로그인
+            </button>
           </form>
         </div>
       </AppLayout>
